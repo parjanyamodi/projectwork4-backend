9c4a9c36e04fba326cb56619f093bc1f00df1aff
Parjanya Modi <parjanya.cs19@bmsce.ac.in>
2022-07-11 23:51:08 +0530

    FILES REFACTORED

--- db.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-
-const mongoose = require("mongoose");
-const uri = process.env.MONGODB_URI;
-
-mongoose.connect(uri).then(() => {
-  console.log("DataBase connection successful");
-});
