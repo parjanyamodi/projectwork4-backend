696e3599cf2034f13cfdb4b1bcefaa3775c1f80c
Parjanya Modi <parjanya.cs19@bmsce.ac.in>
2022-07-12 00:54:45 +0530

    getData route defined

--- controller/data.controller.js
+++ controller/data.controller.js
@@ -1,34 +1,47 @@
-const Data = require("../model/data.model")
-const crypto = require("crypto-js")
+const Data = require("../model/data.model");
+const crypto = require("crypto-js");
 
 const postData = async (req, res) => {
-    try {
-        const insertedData = await Data.findOneAndUpdate(
-            {
-                userId: crypto.SHA256(req.body.email).toString(),
-            },
-            {
-                userId: crypto.SHA256(req.body.email).toString(),
-                Name: req.body.name,
-                Age: req.body.age,
-                ReportYear: req.body.ReportYear,
-                SerumIron: req.body.SerumIron,
-                SerumFerritin: req.body.SerumFerritin,
-                SerumZn: req.body.SerumZn,
-                TSH: req.body.TSH,
-                AbnormalHairShaft: req.body.AbnormalHairShaft,
-                Dandruff: req.body.Dandruff,
-                Hairfall: req.body.Hairfall
-            },
-            {
-                upsert: true,
-                new: true
-            }
-        )
-        res.json({ status: 200, data: insertedData, message: "Updated Successfully" })
-    }
-    catch (e) {
-        res.json({ status: 400, message: "Unexpected Error" })
-    }
-}
-module.exports = { postData }
+  try {
+    const insertedData = await Data.findOneAndUpdate(
+      {
+        userId: crypto.SHA256(req.body.email).toString(),
+      },
+      {
+        userId: crypto.SHA256(req.body.email).toString(),
+        Name: req.body.name,
+        Age: req.body.age,
+        ReportYear: req.body.ReportYear,
+        SerumIron: req.body.SerumIron,
+        SerumFerritin: req.body.SerumFerritin,
+        SerumZn: req.body.SerumZn,
+        TSH: req.body.TSH,
+        AbnormalHairShaft: req.body.AbnormalHairShaft,
+        Dandruff: req.body.Dandruff,
+        Hairfall: req.body.Hairfall,
+      },
+      {
+        upsert: true,
+        new: true,
+      }
+    );
+    res.json({
+      status: 200,
+      data: insertedData,
+      message: "Updated Successfully",
+    });
+  } catch (e) {
+    res.json({ status: 400, message: "Unexpected Error" });
+  }
+};
+const getData = async (req, res) => {
+  try {
+    const newData = await Data.findOne({
+      userId: crypto.SHA256(req.body.email).toString(),
+    });
+    res.json({ status: 200, data: newData, message: "Fetched Successfully" });
+  } catch (e) {
+    res.json({ status: 400, message: "Unexpected Error" });
+  }
+};
+module.exports = { postData, getData };
--- index.js
+++ index.js
@@ -8,9 +8,8 @@ const router = express.Router();
 const Port = process.env.PORT;
 const app = express();
 
-const userController = require("./controller/user.controller")
-const dataController = require("./controller/data.controller")
-
+const userController = require("./controller/user.controller");
+const dataController = require("./controller/data.controller");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -23,18 +22,21 @@ app.use("/", router);
 
 /* List of Routes */
 router.get("/", async (req, res) => {
-    res.json({
-        views: "/views",
-    });
+  res.json({
+    views: "/views",
+  });
 });
 router.post("/login", cors(), async (req, res) => {
-    await userController.loginUser(req, res)
+  await userController.loginUser(req, res);
 });
 router.post("/register", cors(), async (req, res) => {
-    await userController.registerUser(req, res)
+  await userController.registerUser(req, res);
 });
 router.post("/data", cors(), async (req, res) => {
-    await dataController.postData(req, res)
+  await dataController.postData(req, res);
+});
+router.put("/data", cors(), async (req, res) => {
+  await dataController.getData(req, res);
 });
 
 /* Server start */
